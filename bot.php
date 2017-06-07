f9aba786745a4894efda402e9f4146f5f62e9187
janezkk <pichavarit@gmail.com>
2017-06-07 22:21:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,20 +13,17 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
+			 if(strpos($event['message']['text'], 'ขอวีขึ้น') !== false ){
+				// Get text sent
+			//$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			
-			 if(strpos($text, $question) !== false ){
-		    		$messages = [
-				'type' => 'text',
-				'text' =>  'รับ'
-				];
-			 }
 
 			// Build message to reply back
-			
+			$messages = [
+				'type' => 'text',
+				'text' => 'รับ'
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -47,6 +44,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+			 }
 		}
 	}
 }
