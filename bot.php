27137d682cd8ac1525791957757d4d6118da747d
janezkk <pichavarit@gmail.com>
2017-06-07 22:02:24 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,8 +16,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
     if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 	    
-	    if(&& (strpos($event['message']['text'], $question)) 
-	|| strpos($event['message']['text'], $question_one))
+	    if((strpos($event['message']['text'], $question)) || strpos($event['message']['text'], $question_one))
 	    {
 		    $messages = [
 				'type' => 'text',
